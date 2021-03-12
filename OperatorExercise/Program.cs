c57ef9acfef345dfb71899dc2cbfa7cc600bfd7f
Whit Stroup <email@example.com>
2021-03-12 17:38:29 -0600

    yeah

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello jiowfiwe");
+        }
+    }
+}
